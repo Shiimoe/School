a1cc448d4e58c801049ac8fc9d222a346aaf6a52
Shiimoe <SamuelFrost@tuta.io>
2020-02-22 22:48:19 +0000

    updated files

---
+++ Maths/Maths/labels.pl
@@ -0,0 +1,17 @@
+# LaTeX2HTML 2019 (Released January 1, 2019)
+# Associate labels original text with physical files.
+
+
+$key = q/eqn:Total_Derivative/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2019 (Released January 1, 2019)
+# labels from external_latex_labels array.
+
+
+1;
+
